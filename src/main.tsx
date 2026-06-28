c9c4ad31730c17abb06764290d815e0666bf76c3
Saad Alqithami <alqithami@gmail.com>
2026-06-28 03:00:03 +0300

    Load provider health strip styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./tileMap.css";
+import "./statusPanel.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
