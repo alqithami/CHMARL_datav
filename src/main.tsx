a12a30f290c2971ac21e30fb6574ab6f40f0b506
Saad Alqithami <alqithami@gmail.com>
2026-06-30 23:45:43 +0300

    Load vessel table polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./professionalLayout.css";
 import "./insightPanels.css";
 import "./uiClutterPolish.css";
 import "./mapMarkerPolish.css";
+import "./vesselTablePolish.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
