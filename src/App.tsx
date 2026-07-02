82054865a97120edc218489fe16504c3198d5ff0
Saad Alqithami <alqithami@gmail.com>
2026-07-03 00:14:59 +0300

    Restore stable dashboard app mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import DashboardShell from "./components/DashboardShell";
-import MawaniThemeToggle from "./components/MawaniThemeToggle";
 
 export default function App() {
-  return (
-    <>
-      <DashboardShell />
-      <MawaniThemeToggle />
-    </>
-  );
+  return <DashboardShell />;
 }
