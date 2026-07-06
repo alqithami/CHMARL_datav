67515c859a5250005cb96a498847fd7297261112
Saad Alqithami <alqithami@gmail.com>
2026-07-06 05:05:00 +0300

    Mount dark light theme control

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import DashboardShell from "./components/DashboardShell";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function App() {
-  return <DashboardShell />;
+  return (
+    <>
+      <DashboardShell />
+      <ThemeToggle />
+    </>
+  );
 }
