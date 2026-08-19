0b30462fcee017e38db6686aaa8a23cd8c7df6da
Saad Alqithami <alqithami@gmail.com>
2026-08-19 09:37:45 +0300

    Move theme control into the portal header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import DashboardShell from "./components/DashboardShell";
-import ThemeToggle from "./components/ThemeToggle";
 
 export default function App() {
-  return (
-    <>
-      <DashboardShell />
-      <ThemeToggle />
-    </>
-  );
+  return <DashboardShell />;
 }
